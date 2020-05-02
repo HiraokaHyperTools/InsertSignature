0c5abc95165aa19957442bf29442d98700b47cd3
kenjiuno <ku@digitaldolphins.jp>
2020-05-02 16:09:39 +0900

    0.2.0
    - Adding hand made icons.
    - Adjust strict versions.

--- pack.cmd
+++ pack.cmd
@@ -1,3 +1,3 @@
 set XPI=OpenAttachmentByExtension.xpi
 del "%XPI%"
-"C:\Program Files\7-Zip\7z.exe" a "%XPI%" *.* -x!pack.cmd -x!*.xpi -r
+"C:\Program Files\7-Zip\7z.exe" a "%XPI%" *.* -x!pack.cmd -x!*.xpi -x!.git -x!*.pxf -x!*.dxf -r
